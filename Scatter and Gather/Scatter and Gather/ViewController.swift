bde6dfc2468e30fe86da3678959c5ba5f3abe459
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-11-17 21:20:43 -0500

    Initial Commit

---
+++ Scatter and Gather/Scatter and Gather/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Scatter and Gather
+//
+//  Created by Christy Hicks on 11/17/19.
+//  Copyright © 2019 Knight Night. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Property
+    
+    var isScattered = false
+    
+    // MARK: - View
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Action
+    @IBAction func toggleButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+}
